2de17691babcab62f411fb17d7149c3247465501
totallynotisla <wkomangadi44@gmail.com>
2026-02-08 00:31:28 +0800

    pertemuan 10

---
+++ pert10/script.js
@@ -0,0 +1,32 @@
+// //For Loop
+// for (let i = 0; i < 10; i++) {
+// 	console.log(`For loop ke-${i + 1}`);
+// }
+
+// //While Loop
+// let i = 0;
+// while (i < 10) {
+// 	console.log(`While loop ke-${i + 1}`);
+// 	i++;
+// }
+
+// //Do While Loop
+// let j = 0;
+// do {
+// 	console.log(`Do While loop ke-${j + 1}`);
+// 	j++;
+// } while (j < 10);
+
+// function maximum(a, b, callback) {
+// 	if (a > b) {
+// 		callback(a);
+// 	} else {
+// 		callback(b);
+// 	}
+// }
+
+// maximum(10, 20, (hasil) => {
+// 	console.log(hasil);
+// });
+
+// console.log(maximum(10, 20));
